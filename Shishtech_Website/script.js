56dd92ca31d44adf060a9e7941af22c39352c126
AaravGarg16 <aaravgarg1000@gmail.com>
2022-10-08 20:24:06 +0530

    new file:   Shishtech_Website/assets/img/avatar/form.jpg 	modified:   Shishtech_Website/form.html

--- Shishtech_Website/script.js
+++ Shishtech_Website/script.js
@@ -1,19 +1,12 @@
-var elem = document.getElementsByClassName('cursor'),
-    canTouch = !!('ontouchstart' in window);
-
-
-if(canTouch){
-  document.body.addEventListener('touchmove', onTouchStart, false);
-  document.body.addEventListener('touchstart', onTouchStart, false);
-}else{
-  document.body.addEventListener('mousemove', onMoseMove, false);
-}
-
-function onTouchStart(e){
-  var target = e.targetTouches[0];
-  elem.style.backgroundImage  = '-webkit-radial-gradient(' + target.pageX + 'px ' + target.pageY + 'px, circle cover, rgba(0,0,0,0.2) 5%, rgba(0,0,0,1) 25%)';
-}
-
-function onMoseMove(e) {
-    elem.style.backgroundImage  = '-webkit-radial-gradient(' + e.pageX + 'px ' + e.pageY + 'px, circle cover, rgba(0,0,0,0.2) 5%, rgba(0,0,0,1) 25%)';
-}
+$(document).mousemove(function(e) {
+    var X = e.pageX;
+    var Y = e.pageY;
+    $(".cursor").css(
+      "background",
+      "radial-gradient(circle at " +
+        X +
+        "px " +
+        Y +
+        "px,transparent, white 30%)"
+    );
+  });
